60c309734c2d87c3c7b85954e8d79fbd15ca53a8
Laura Sainz Villalba <78734663+lsainzvillalba@users.noreply.github.com>
2026-03-04 10:53:32 +0100

    Create __init__.py

---
+++ single_neuron/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 16:08:17 2025
+
+@author: Laura Sainz Villalba
+"""
+
+from .zeta_modulated import Neuron_modulated,Neuron_peths,Labels
