0bc32b9f5a5098bb812041f92c19107930c8ba8b
Laura Sainz Villalba <78734663+lsainzvillalba@users.noreply.github.com>
2026-03-04 10:51:15 +0100

    Create __init__.py

---
+++ behaviour/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 16:08:17 2025
+
+@author: lsainz
+"""
+from .training import *
+from .logreg import Logreg_coefs
+from .psychometrics import Psychometric_curves
+from .day_statistics import Day
