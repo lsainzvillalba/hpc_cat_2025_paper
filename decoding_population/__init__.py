f8c9612e7d66173b9dd9474ed632b2ccba98845d
Laura Sainz Villalba <78734663+lsainzvillalba@users.noreply.github.com>
2026-03-04 10:54:38 +0100

    Create __init__.py

---
+++ decoding_population/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 16:08:17 2025
+
+@author: Laura Sainz Villalba
+"""
+from .pseudopopulation_intime import Single_var, Behavior
+from .pseudopopulation_geometry import Cross_var, By_window, By_window_decoder,\
+    Window_cohort_neuronset,Window_cohort_decspace
+from .subject_decoding import Single_var_subject,Cross_var_subject,Window_subject,\
+    Window_subject_decoder
