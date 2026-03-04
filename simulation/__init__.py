636dd9b15516fe2e7bdf15909bce4106ed2c41f7
Laura Sainz Villalba <78734663+lsainzvillalba@users.noreply.github.com>
2026-03-04 10:58:38 +0100

    Create __init__.py

---
+++ simulation/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 19 16:08:17 2025
+
+@author: Laura Sainz Villalba
+"""
+from .simulation_results import Simulation_forstats, Simulation_neuralspace
